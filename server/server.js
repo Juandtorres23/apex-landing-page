dc9e886e4688b89a32988fc62b49ef49549f6f76
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-09-13 21:14:37 -0400

    ready for deployment

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app. use(cors());
+app.use(express.json());
+
+//middleware
+
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
+});
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`I think we are on port ${port}`);
+});
+
